0aa128077d234eef03e85fa392a3b5fa45758a66
Rodrigo-Ornellas <rodrigo.ornellas@mail.com>
2018-10-09 13:26:49 -0700

    salsa: Android App that calls a second activit

---
+++ w1_salsa/app/src/main/java/com/example/rod/salsa/recipe.java
@@ -0,0 +1,13 @@
+package com.example.rod.salsa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
